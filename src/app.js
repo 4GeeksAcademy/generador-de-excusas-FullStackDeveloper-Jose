87506d99ec891e34cf16e0cda5b2bf0e4395faff
rodriguezmontillajose39-dotcom <rodriguezmontillajose39@gmail.com>
2025-12-30 04:13:15 +0000

    <Generador de excusas 1.0>

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+  let Titulo = document.getElementById("Titulo")
+  Titulo.innerHTML = "This Page Doesn't Onload Because...";
+  let excusas = document.getElementById("excusas")
+  excusas.innerHTML = ""
+  let sorry = document.getElementById("sorry")
+  sorry.innerHTML= "Try again later..."
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let RandomWho = Math.floor(Math.random()* who.length);
+  console.log(RandomWho)
+  let RandomAction = Math.floor(Math.random()* action.length);
+  console.log(RandomAction)
+  let RandomWhat = Math.floor(Math.random()* what.length);
+  console.log(RandomWhat)
+  let RandomWhen = Math.floor(Math.random()* when.length);
+  console.log(RandomWhen)
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  excusas.innerHTML = who[RandomWho]+" "+ action[RandomAction]+" "+ what[RandomWhat]+" "+ when[RandomWhen];
 };
